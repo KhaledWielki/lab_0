f903b02594477af1a1f60c9927be194dd8793556
Rezner.Adam <195047@edu.p.lodz.pl>
2017-02-28 09:16:20 -0800

    Naprawienie funkcji tails

--- src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
+++ src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
@@ -4,10 +4,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class TailsGenerator {
+	
 
-	public List<String> tails(String value) {
-		List<String> tails = new ArrayList<>(value.length());		
+	static List<String> tails(String value) {
+		List<String> tails = new ArrayList<>(value.length());
+		for(int i = 0; i <= value.length(); i++) {
+			tails.add(value.substring(i));
+		}
+		System.out.println(tails);
 		return tails;
 	}
-
+	
 }
